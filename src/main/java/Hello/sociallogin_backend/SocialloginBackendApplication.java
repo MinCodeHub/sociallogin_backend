1284320a7ed186db7e18223f80355d05561fbadc
dev_me <gjalsdud1030@naver.com>
2024-04-05 16:45:28 +0900

    first commit

---
+++ src/main/java/Hello/sociallogin_backend/SocialloginBackendApplication.java
@@ -0,0 +1,13 @@
+package Hello.sociallogin_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialloginBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialloginBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hello/sociallogin_backend/SocialloginBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Hello.sociallogin_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialloginBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
